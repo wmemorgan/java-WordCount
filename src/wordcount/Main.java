0161c39f0924aabc8d8e6cdad689cf14aabbc9ea
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-06-10 19:11:02 +0800

    Add final display format

--- src/wordcount/Main.java
+++ src/wordcount/Main.java
@@ -8,27 +8,33 @@ public class Main {
 
         // Parse text
         String parsedText = untext.replaceAll("[[\\.\\?\\!\\,\\;\\:\\-\\{\\}\\(\\)\\']]", "").toUpperCase();
-        System.out.println("\nOriginal text: " + untext);
+        // System.out.println("\nOriginal text: " + untext);
 
-        System.out.println("\nRemove punctuation: " + parsedText);
+        // System.out.println("\nRemove punctuation: " + parsedText);
 
         String[] words = parsedText.split(" +");
-        System.out.println("\nSplit string into words: ");
+        // System.out.println("\nSplit string into words: ");
 
         // Create and populate hashmap
         Map<String, Integer> wordMap = new HashMap<>();
         for (String word : words) {
             int count = wordMap.containsKey(word) ? wordMap.get(word) : 0;
             wordMap.put(word, count + 1);
-            System.out.println(word);
         }
-        System.out.println("\n wordMap: " + wordMap);
+        // System.out.println("\n wordMap: " + wordMap);
 
         // Sort hashmap
         // Convert hashmap to ArrayList
         List<HashMap.Entry> sortedWordMap = new ArrayList<>(wordMap.entrySet());
         sortedWordMap.sort(Comparator.comparing(o -> ((int)o.getValue()), Comparator.reverseOrder()));
-        System.out.println("\n*** Sorted Word List ***");
-        System.out.println(sortedWordMap);
+        // System.out.println("\n*** Sorted Word List ***");
+        // System.out.println(sortedWordMap);
+
+        // Print contents of sorted hashmap
+        int limit = sortedWordMap.size();
+        System.out.println("\nTop " + limit + " words\n");
+        for (HashMap.Entry mapElem : sortedWordMap) {
+            System.out.println("Common Word <" + mapElem.getKey() + "> occurs " + mapElem.getValue() + " times");
+        }
     }
 }
