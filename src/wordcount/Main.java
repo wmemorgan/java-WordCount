a020e518fa50a66e46d9fc6a0c46ee0b8c97b399
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-06-10 19:59:24 +0800

    Print the top 50 appearing words alphabetically

--- src/wordcount/Main.java
+++ src/wordcount/Main.java
@@ -23,14 +23,29 @@ public class Main {
 
         // Sort hashmap
         // Convert hashmap to ArrayList
+        // Sort by word frequency in descending order
         List<HashMap.Entry> sortedWordMap = new ArrayList<>(wordMap.entrySet());
-        sortedWordMap.sort(Comparator.comparing(o -> ((int)o.getValue()), Comparator.reverseOrder()));
+        sortedWordMap.sort(Comparator.comparing(o -> ((int) o.getValue()), Comparator.reverseOrder()));
+        // Sort alphabetically
+        List<HashMap.Entry> alphaSortWordMap = new ArrayList<>(wordMap.entrySet());
+        alphaSortWordMap.sort(Comparator.comparing(o -> ((String) o.getKey())));
+        
 
         // Print contents of sorted hashmap
         int limit = 50;
         int count = 1;
+        // Print by word frequency
         System.out.println("\nTop " + limit + " words\n");
         for (HashMap.Entry mapElem : sortedWordMap) {
+            if (count > limit)
+                break;
+            System.out.println("Common Word <" + mapElem.getKey() + "> occurs " + mapElem.getValue() + " times");
+            count++;
+        }
+        // Print alphabetically
+        System.out.println("\n*** Sorted Alphabetically ***");
+        count = 1;
+        for (HashMap.Entry mapElem : alphaSortWordMap) {
             if (count > limit) break;
             System.out.println("Common Word <" + mapElem.getKey() + "> occurs " + mapElem.getValue() + " times");
             count++;
